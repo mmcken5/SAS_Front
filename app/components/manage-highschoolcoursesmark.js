7a11d4267d939070e51c3334ab5fa9797fe1d472
Mike <mmcken5@uwo.ca>
2016-04-02 17:10:00 -0400

    able to add a secondary school, subject, and mark

--- app/components/manage-highschoolcoursesmark.js
+++ app/components/manage-highschoolcoursesmark.js
@@ -3,28 +3,37 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     isHighschoolcoursesmarkFormEditing: false,
     store: Ember.inject.service(),
-    selectedStudent: null,
-    //selectedStudent: null,
+    selectedSchool: null,
+    selectedSubject: null,
 
-    studentModel: Ember.computed(function(){
+    secondarySchoolModel: Ember.computed(function(){
       var self = this;
-        this.get('store').findAll('student').then(function(records){
-        self.set ('selectedStudent', records.get('firstObject').get('id'));
+        this.get('store').findAll('secondaryschool').then(function(records){
+        self.set ('selectedSchool', records.get('firstObject').get('id'));
       });
-      return this.get('store').findAll('student');
+      return this.get('store').findAll('secondaryschool');
+    }),
+
+    highSchoolSubjectModel: Ember.computed(function(){
+      var self = this;
+        this.get('store').findAll('highschoolsubject').then(function(records){
+        self.set ('selectedSubject', records.get('firstObject').get('id'));
+      });
+      return this.get('store').findAll('highschoolsubject');
     }),
 
     actions: {
       saveHighschoolcoursesmark () {
         var myStore = this.get('store');
-        var chosenStudent = myStore.peekRecord('student', this.get ('selectedStudent'));
+        var chosenSchool = myStore.peekRecord('secondaryschool', this.get ('selectedSchool'));
+        var chosenSubject = myStore.peekRecord('highschoolsubject', this.get ('selectedSubject'));
         var newHighschoolcoursesmark = myStore.createRecord('highschoolcoursesmark', {
           level: this.get('level'),
           source: this.get('source'),
           unit: this.get('unit'),
           grade: this.get('grade'),
-          secondaryschool: chosenStudent//,
-          //highshchoolsubject: 
+          secondaryschool: chosenSchool,
+          highschoolsubject: chosenSubject
         });
 
         newHighschoolcoursesmark.save().then(() => {
@@ -32,9 +41,14 @@ export default Ember.Component.extend({
         });
       },
 
-      selectStudent (student){
-        this.set('selectedStudent', student);
-        Ember.Logger.log(this.get('selectedStudent'));
+      selectSchool (secondarySchool){
+        this.set('selectedSchool', secondarySchool);
+        Ember.Logger.log(this.get('selectedSchool'));
+      },
+
+      selectSubject (subject){
+        this.set('selectedSubject', subject);
+        Ember.Logger.log(this.get('selectedSubject'));
       },
 
       addHighschoolcoursesmark () {
